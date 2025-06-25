ab184468cca455b0b97366a349c46295a1d026c1
Minhaz Faisal <faisalahmed01944@gmail.com>
2025-06-25 15:31:16 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// js
+
+// toggle search
+
+function showHS(s_form){
+
+	const selectorId= document.getElementById('s_form');
+
+	if( selectorId.style.display == "block" ){
+		selectorId.style.display = "none";
+	}else{
+		selectorId.style.display = "block";
+	}
+
+}
